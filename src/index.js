e499662dbf32a21694f741e5a44b89e3abafca5b
lehoangan <22.95score@gmail.com>
2025-10-03 00:39:46 +0700

    Add boostrap

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// app.js
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("combined"));
+
+// cấu hình Handlebars
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "resources/views/layouts"),
+    partialsDir: path.join(__dirname, "resources/views/partials"),
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+// route demo render view
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
